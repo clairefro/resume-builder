150e26d2154a93612678c5405015359c0e043813
claire froelich <claire.froelich@gmail.com>
2020-05-18 15:20:23 -0400

    Updates

---
+++ precache-manifest.33038bd0d78ed8c04871460bccae6d1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7582cd2797c85850a2579713efeb1ee2",
+    "url": "/resume-builder/index.html"
+  },
+  {
+    "revision": "2a7050dfe2f175df8e00",
+    "url": "/resume-builder/static/css/main.42eb91af.chunk.css"
+  },
+  {
+    "revision": "6298c057173f28df3fb4",
+    "url": "/resume-builder/static/js/2.62321183.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume-builder/static/js/2.62321183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7050dfe2f175df8e00",
+    "url": "/resume-builder/static/js/main.d66d03fe.chunk.js"
+  },
+  {
+    "revision": "9420dd8b4dfc71a7a424",
+    "url": "/resume-builder/static/js/runtime-main.4d81b245.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume-builder/precache-manifest.681da81ce07beba02e7acff5d74fe46e.js"
+  "/resume-builder/precache-manifest.33038bd0d78ed8c04871460bccae6d1d.js"
 );
 
 self.addEventListener('message', (event) => {
