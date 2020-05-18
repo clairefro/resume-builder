a87a3b335b58777e57653a84c4e8f02b7122c3ed
claire froelich <claire.froelich@gmail.com>
2020-05-18 15:11:29 -0400

    json editor

--- src/components/resumeGenerator.js
+++ src/components/resumeGenerator.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { JsonEditor } from 'react-json-edit';
 
 import ResumePreview from './resumePreview';
 
@@ -11,12 +12,16 @@ const ResumeGenerator = () => {
     e.preventDefault();
     const input = document.getElementById('input-fetch');
     console.log(input.value)
-    fetch(input.value).then(res => res.json()).then(data=> console.log(data))
+    fetch(input.value).then(res => res.json()).then(data=> setData(data))
   }
-
+  const update = (newData) => {
+    setData(newData)
+  }
+  
   return (
     <div className="resume-generator">
       <h1>Resume Builder</h1>
+      <JsonEditor value={data} propagateChanges={update} />
       <form onSubmit={handleFetch}>
         <input type="text" id="input-fetch" placeholder="enter data endpoint" />
         <input type="submit" value="Fetch data"/>
