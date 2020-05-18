f544358c91444e7028833f955b1a50bd0fb4c908
claire froelich <claire.froelich@gmail.com>
2020-05-18 12:56:55 -0400

    adding image

--- src/components/resumeGenerator.js
+++ src/components/resumeGenerator.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import ResumePreview from './resumePreview';
+
+import { data } from '../data/sample';
+
 const ResumeGenerator = () => (
   <div className="resume-generator">
-  
+    <h1>Resume Builder</h1>
+
+    <ResumePreview data={data} />
   </div>
 )
 
-export default ResumeGenerator
+export default ResumeGenerator;
